f23d8e276cec6653dd7b0593198f02e25c0b050f
malachieborohoul <malachieborohoul@gmail.com>
2024-04-10 17:59:42 +0100

    Initialize Hive and Open boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:facturo/constants/color.dart';
 import 'package:facturo/features/invoice/screens/invoice_screen.dart';
+import 'package:facturo/models/client.dart';
+import 'package:facturo/models/invoice.dart';
+import 'package:facturo/models/item.dart';
+import 'package:facturo/models/item_type.dart';
 import 'package:facturo/router.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+
+void main() async{
+    // Initialize hive before MyApp runs
+  WidgetsFlutterBinding.ensureInitialized();
+  // Provide path
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ClientAdapter());
+  Hive.registerAdapter(InvoiceAdapter());
+  Hive.registerAdapter(ItemAdapter());
+  Hive.registerAdapter(ItemTypeAdapter());
+  // Open boxes
+  await Hive.openBox("invoices");
+  await Hive.openBox("clients");
+  await Hive.openBox("items");
+  await Hive.openBox("item_types");
   runApp(const MyApp());
 }
 
